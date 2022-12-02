c1381ff1d8b72ac797ce3d080d03bad1fcb934aa
yelim <yelim785@gmail.com>
2022-12-02 11:01:09 +0900

    community 앱 클래스형 뷰 완성

--- community/urls.py
+++ community/urls.py
@@ -5,5 +5,5 @@ app_name = 'community'
 urlpatterns = [
     path('write/', views.WriteFormView.as_view(), name='write'),
     path('list/', views.ArticleListView.as_view(), name='list'),
-    path('view_detail/<int:num>/', views.ArticleDetailView.as_view(), name='view_detail'),
+    path('view_detail/<slug:pk>/', views.ArticleDetailView.as_view(), name='view_detail'),
 ]
--- community/views.py
+++ community/views.py
@@ -28,18 +28,16 @@ from django.views.generic import CreateView, ListView, DetailView
 #     article_detail = Article.objects.get(id=num) # 하나 가져오기
 #     return render(request, 'community/view_detail.html', {'article_detail':article_detail})
 
-def index(request):
-    latest_article_list = Article.objects.all().order_by('-cdate')[:3] # 내림차순으로 3개 정렬하기 
-    return render(request, 'index.html', {'latest_article_list' : latest_article_list})
-
 class WriteFormView(CreateView):
     model = Article
     fields = ['name', 'title', 'contents', 'url', 'email']
     template_name = 'community/write.html'
-    success_url = reverse_lazy('community:article_list')
+    success_url = reverse_lazy('community:list')
 
     # 부모 클래스인 CreateView 클래스의 form_valid() 호출을 위해 super()
     # form.save() 실행과 success_url로 리다렉트
+    # 유효한 폼 데이터가 POST 요청되었을 때, form_valid가 호출된다.
+    # form_valid는 단순히 success_url로의 연결을 수행 
     def form_valid(self, form):
         return super().form_valid(form)
 
@@ -50,3 +48,7 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'community/view_detail.html'
+
+def index(request):
+    latest_article_list = Article.objects.all().order_by('-cdate')[:3] # 내림차순으로 3개 정렬하기 
+    return render(request, 'index.html', {'latest_article_list' : latest_article_list})
